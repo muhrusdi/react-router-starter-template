5d36a21422a09245bd803fbf3414a5a175fd6067
muhrusdi <muhammadrusdiid@gmail.com>
2025-10-18 11:20:34 +0700

    Remove SSR configuration and add prerendering support

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,8 +1,10 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-  ssr: true,
   future: {
     unstable_viteEnvironmentApi: true,
+  },
+  async prerender() {
+    return ["/"];
   },
 } satisfies Config;
